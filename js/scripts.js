5435c50c67635205ee53aaa4241cac0f54c23c43
nic <netzeln@gmail.com>
2016-01-15 10:19:05 -0800

    make working form values

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+  $("form.survey").submit(function(event){
+    var usa = $("input#usa").prop('checked');
+    var distance = $("#distance").val();
+    var spend = $("input[name='optionsSpend']:checked").val();
+    var season = $("input[name='optionsSeason']:checked").val();
+    var beaches = parseInt($("#beaches").val());
+    var snow = $("input#checkSnow").prop('checked');
+console.log(spend)
+         $(".TEST").text(usa +' '+ distance +' '+ spend +' '+ season +' '+ beaches +' '+ snow);
+   event.preventDefault();
+  });
 
 
 });
