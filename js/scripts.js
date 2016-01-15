1ca6adaeaa05238452e3a3f3ad9176e7616bd0a1
nic <netzeln@gmail.com>
2016-01-15 15:31:56 -0800

    adjust styling

--- js/scripts.js
+++ js/scripts.js
@@ -19,43 +19,74 @@ $(document).ready(function() {
       if ((usa === true && distance === "no") || (usa === false && distance !== "no")){
          if (beaches >= 2 && snow === false){
            if(spend <=  2 && season === "summer") {
-             var destination = "Duluth";
+             var destination = "Duluth, MN";
+             var simpleDest = "DLH";
          } else if (spend >= 2 && beaches >= 3) {
-            var destination = "Hawaii";
+            var destination = "Honolulu, Hawaii";
+            var simpleDest = "HNL";
          } else {
-           var destination = "Los Angeles"
+           var destination = "Los Angeles, CA"
+           var simpleDest = "LAX";
          }
 
        } else if (snow === false && season === "notSummer"){
-           var destination = "Kansas City";
+           var destination = "Kansas City, MO";
+           var simpleDest = "MCI";
          }else {
-           var destination = "Duluth";
+           var destination = "Duluth, MN";
+           var simpleDest = "DLH";
          }
 
       }else {
         if (beaches >=3){
-          var destination ="Barcelona"
-        }
-         else if (beaches === 2){
-          if (spend >= 2 ){
-          var destination = "Copenhagen";}
-          else {
-          var destination = "Barcelona"
+          var destination ="Barcelona, Spain"
+          var simpleDest = "BCN";
+        }else if (beaches === 2){
+           if (spend >= 2 ){
+             var destination = "Copenhagen, Denmark";
+             var simpleDest = "CPH";
+         } else {
+             var destination = "Barcelona, Spain"
+             var simpleDest = "BCN";
           }
         }
         else{
           if (spend >= 2 && snow === false && season === "notSummer"){
-           var destination = "London"
+            var destination = "London, UK";
+            var simpleDest = "LHR";
          } else if(spend >= 2 || (spend >= 1 && season === "notSummer")) {
-           var destination = "Stockholm"
+            var destination = "Stockholm, Sweden";
+            var simpleDest = "ARN";
          } else{
-          var destination = "Prague"
+          var destination = "Prague, Czech Republic";
+          var simpleDest = "PRG";
+          $(".aboutDestination").empty().append("Based on your selections we recommend you go to:... <span class='destination'>" + destination + "</span>!"  );
         }
         }
       }
 
     $("#resultText").show();
-    $(".yourResult").text("Go to " + " "+ distance + " "+ destination + " " + spend)
+    $(".yourResult").empty().append("Based on your selections we recommend you go to:... <span class='destination'>" + destination + "</span>!" + simpleDest );
+
+    if (simpleDest === "DLH") {
+      $("#aboutDestination").empty().html("Duluth is truly a 'cool' city. Boasting 'Park Point,' the worlds longest freshwater sandbar, it has almost 5 miles of sand beaches. Recently named Outside Magazine's 'outdoor city of the year,' it is a biker or paddler's paradise in the summer, and a winter sports enthusiast's playground in the winter</p>");
+    } else if (simpleDest === "PRG") {
+      $("#aboutDestination").empty().html("Settled farther in the continent, Prague has <ul><li>Charming Architecture</li><li>Lots of Classical Music, AND</li><li><em>Cheap</em> Beer!</li></ul> ");
+    }else if (simpleDest === "MCI") {
+      $("#aboutDestination").empty().html("Kansas City is an overlooked gem in the heartland of the US. It Boasts <ul><li>The Royals</li><li>Lots of fountains, AND</li><li><em>Amazing</em> Barbecue!</li></ul> ");
+    }else if (simpleDest === "LHR") {
+      $("#aboutDestination").empty().html("London can be expensive, but it's one of the great cultural cities of the world. You can:<ul><li>See London Bridge</li><li>See the Tower of London, AND</li><li>Pretend to be Sherlock Holmes</li></ul> ");
+    }else if (simpleDest === "BCN") {
+      $("#aboutDestination").empty().html("Barcelona is probably the best City in Europe. It has many fantastic things<ul><li>Amazing Architecture</li><li>Great Museums,</li><li><em>Many</em> Great Beaches!</li><li>and, most importantly:<strong>Jamon Iberico de Bellota</strong></ul> ");
+    }else if (simpleDest === "CPH") {
+      $("#aboutDestination").empty().html("Almost as great as Barcelona,  Copenhagen is an amazing city with a lot to offer. <ul><li>Amazing Restaurants</li><li>Kayaking, Biking, Walking</li><li> HOWEVER, it's quite <em>Expensive</em> !</li></ul> ");
+    }else if (simpleDest === "LAX") {
+      $("#aboutDestination").empty().html("California has a bit of everything, and is great year round<ul><li>Beaches</li><li>Movie Stars,AND</li><li> it's not far from<em>Disneyland</em>!</li></ul> ");
+    }else if (simpleDest === "ARN") {
+      $("#aboutDestination").empty().html("Stockholm, while expensive, is a beautiful city built on an archipelago. It can get cold in the winter. <ul><li>Outdoor Museums</li><li>IKEA, AND</li><li><em>Cheap</em> Beer!</li></ul> ");
+    }
+
+
    event.preventDefault();
   });
 
